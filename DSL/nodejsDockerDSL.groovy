b06a8b0cd927f9d03e343263cec6a8373f26083b
alexmfz <47566168+alexmfz@users.noreply.github.com>
2024-11-11 14:14:36 +0100

    Update nodejsDockerDSL.groovy

--- DSL/nodejsDockerDSL.groovy
+++ DSL/nodejsDockerDSL.groovy
@@ -16,7 +16,7 @@ job('Aplicacion Node.js Docker DSL') {
         dockerBuildAndPublish {
             repositoryName('nataelmf/nodejsapp')
             tag('${GIT_REVISION,length=7}')
-            registryCredentials('Sephiroth$_98')
+            registryCredentials('72d07aeb-98b3-4b4a-ae4f-38cef5aed3d8')
             forcePull(false)
             createFingerprints(false)
             skipDecorate()
